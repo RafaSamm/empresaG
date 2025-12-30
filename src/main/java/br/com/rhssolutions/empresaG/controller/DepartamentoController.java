efcefe46a18b36637e605da5759a1afd9adcc9f4
Rafael Sammarco <126182047+RafaSamm@users.noreply.github.com>
2025-12-30 11:31:23 -0300

    feat: Resposta padronizada dos controllers para o front - departamento

--- src/main/java/br/com/rhssolutions/empresaG/controller/DepartamentoController.java
+++ src/main/java/br/com/rhssolutions/empresaG/controller/DepartamentoController.java
@@ -1,6 +1,7 @@
 package br.com.rhssolutions.empresaG.controller;
 
 import br.com.rhssolutions.empresaG.domain.model.departamento.Departamento;
+import br.com.rhssolutions.empresaG.dto.ApiResponse;
 import br.com.rhssolutions.empresaG.dto.DepartamentoDTO;
 import br.com.rhssolutions.empresaG.dto.mapper.DepartamentoMapper;
 import br.com.rhssolutions.empresaG.service.DepartamentoService;
@@ -8,6 +9,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/departamento")
@@ -20,38 +23,61 @@ public class DepartamentoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DepartamentoDTO> buscarDepartamentoPorId(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<DepartamentoDTO>> buscarDepartamentoPorId(@PathVariable Long id) {
         Departamento departamento = departamentoService.buscarDepartamentoPorId(id);
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(DepartamentoMapper.departamentoToDTO(departamento));
-
+        ApiResponse<DepartamentoDTO> response = new ApiResponse<>(
+                HttpStatus.OK.value(),
+                "Departamento encontrado com este ID",
+                DepartamentoMapper.departamentoToDTO(departamento),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/criar/empresa/{empresaId}")
-    public ResponseEntity<DepartamentoDTO> criarDepartamento(@PathVariable Long empresaId, @Valid @RequestBody DepartamentoDTO dto) {
+    public ResponseEntity<ApiResponse<DepartamentoDTO>> criarDepartamento(@PathVariable Long empresaId, @Valid @RequestBody DepartamentoDTO dto) {
         Departamento departamento = DepartamentoMapper.dtoToDepartamento(dto, null);
 
         Departamento salvar = departamentoService.criarDepartamento(empresaId, departamento);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(DepartamentoMapper.departamentoToDTO(salvar));
 
+        ApiResponse<DepartamentoDTO> response = new ApiResponse<>(
+                HttpStatus.CREATED.value(),
+                "Departamento criado com sucesso",
+                DepartamentoMapper.departamentoToDTO(salvar),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PutMapping("/atualizar/{id}")
-    public ResponseEntity<DepartamentoDTO> atualizarDepartamento(@PathVariable Long id, @Valid @RequestBody DepartamentoDTO dto) {
+    public ResponseEntity<ApiResponse<DepartamentoDTO>> atualizarDepartamento(@PathVariable Long id, @Valid @RequestBody DepartamentoDTO dto) {
         Departamento departamento = DepartamentoMapper.dtoToDepartamento(dto, null);
 
         Departamento atualizado = departamentoService.atualizarDepartamento(id, departamento);
 
-        return ResponseEntity.ok().body(DepartamentoMapper.departamentoToDTO(atualizado));
+        ApiResponse<DepartamentoDTO> response = new ApiResponse<>(
+                HttpStatus.OK.value(),
+                "Departamento atualizado com sucesso",
+                DepartamentoMapper.departamentoToDTO(atualizado),
+                LocalDateTime.now()
+        );
 
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletarDepartamento(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<Void>> deletarDepartamento(@PathVariable Long id) {
         departamentoService.deletarDepartamento(id);
-        return ResponseEntity.noContent().build();
+
+        ApiResponse<Void> response = new ApiResponse<>(
+                HttpStatus.NO_CONTENT.value(),
+                "Departamento deletado com sucesso",
+                null,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.ok(response);
+
 
     }
 }
